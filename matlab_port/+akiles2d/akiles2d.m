0ebb16206773cfe389782f94f50a91c07053b8fa
Ioan Sturzu <ioansturzu@gmail.com>
2025-12-11 23:45:42 -0600

    shift to using akiles2d.logger instead of importing

--- matlab_port/+akiles2d/akiles2d.m
+++ matlab_port/+akiles2d/akiles2d.m
@@ -17,7 +17,7 @@ function [data,solution] = akiles2d(simrcfile,userdata)
 
 %% Code version (must update manually when codebase changes)
 VERSION = '20200901';
-import akiles2d.logger.*
+% import akiles2d.logger.* removed
 
 %% Run data preprocessor to prepare data structure
 if ~exist('simrcfile','var')
@@ -33,9 +33,9 @@ data  = akiles2d.preprocessor.preprocessor(simrcfile,userdata);
 save(data.akiles2d.datafile,'data');
 
 %% Startup messages
-logger.title(['AKILES2D version ',VERSION],10,data.logger);
-logger.log(['Simulation directory: ',data.akiles2d.simdir],'INF',5,data.logger);
-logger.log(['Structure data saved to ',data.akiles2d.datafile,' successfully.'],'INF',5,data.logger);
+akiles2d.logger.title(['AKILES2D version ',VERSION],10,data.logger);
+akiles2d.logger.log(['Simulation directory: ',data.akiles2d.simdir],'INF',5,data.logger);
+akiles2d.logger.log(['Structure data saved to ',data.akiles2d.datafile,' successfully.'],'INF',5,data.logger);
 
 %% Initial guess error and save iteration 0
 solution = data.guess; % take guess as initial solution
@@ -50,17 +50,17 @@ for iiter = 0:data.akiles2d.maxiter
     %% Save current solution to file and log message
     solutionfile = fullfile(data.akiles2d.simdir,[num2str(iiter),'.mat']);
     save(solutionfile,'-struct','solution');
-    logger.log(['Iteration ',num2str(iiter),' saved to disk.'],'INF',5,data.logger);
-    logger.log(['Error norm: ',num2str(normerror)],'INF',4,data.logger); 
-    logger.log('Detailed error vector:','INF',1,data.logger);
-    logger.write(num2str(solution.errorfcn),1,data.logger);
+    akiles2d.logger.log(['Iteration ',num2str(iiter),' saved to disk.'],'INF',5,data.logger);
+    akiles2d.logger.log(['Error norm: ',num2str(normerror)],'INF',4,data.logger); 
+    akiles2d.logger.log('Detailed error vector:','INF',1,data.logger);
+    akiles2d.logger.write(num2str(solution.errorfcn),1,data.logger);
 
     %% Check convergence
     if norm(solution.errorfcn) < data.akiles2d.tolerance
-        logger.log(['Convergence reached successfully at iteration ',num2str(iiter),'.'],'INF',5,data.logger);
+        akiles2d.logger.log(['Convergence reached successfully at iteration ',num2str(iiter),'.'],'INF',5,data.logger);
          solutionfile = fullfile(data.akiles2d.simdir,'final.mat');
          save(solutionfile,'-struct','solution');
-         logger.log(['Final iteration saved to ',solutionfile],'INF',5,data.logger);
+         akiles2d.logger.log(['Final iteration saved to ',solutionfile],'INF',5,data.logger);
         break;
     end
     
@@ -70,21 +70,21 @@ end
 
 %% Warn if maximum number of iterations was reached after the loop
 if iiter == data.akiles2d.maxiter
-    logger.log('Maximum number of iterations was reached!','WRN',8,data.logger);
+    akiles2d.logger.log('Maximum number of iterations was reached!','WRN',8,data.logger);
 end
 
 %% Run postprocessor functions and save
 postfunctions = data.postprocessor.postfunctions;
 for ifn = 1:length(postfunctions)
-    logger.log(['Running postprocessor function: ',postfunctions{ifn}],'INF',5,data.logger);        
+    akiles2d.logger.log(['Running postprocessor function: ',postfunctions{ifn}],'INF',5,data.logger);        
     solution = akiles2d.postprocessor.(postfunctions{ifn})(data,solution);
 end
 solutionfile = fullfile(data.akiles2d.simdir,'post.mat');
 save(solutionfile,'-struct','solution');    
-logger.log(['Solution saved to ',solutionfile],'INF',5,data.logger);
+akiles2d.logger.log(['Solution saved to ',solutionfile],'INF',5,data.logger);
 
 %% Farewell
-logger.title('AKILES2D execution finished.',10,data.logger);
+akiles2d.logger.title('AKILES2D execution finished.',10,data.logger);
 
 
 
